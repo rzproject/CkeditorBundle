ff6272ed1569d14c713be5e5896a447228170b02
Mell Zamora <mellzamora@outlook.com>
2013-05-23 18:07:25 +0800

    added uncompressed version of ckeditor

--- DependencyInjection/RzCkeditorExtension.php
+++ DependencyInjection/RzCkeditorExtension.php
@@ -35,6 +35,7 @@ class RzCkeditorExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('ckeditor.xml');
+        $loader->load('twig.xml');
         $this->registerCkeditor($config, $container);
     }
     /**
---
+++ Twig/CkeditorExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Rz\CkeditorBundle\Twig;
+
+use Rz\CkeditorBundle\Model\ConfigManagerInterface;
+use Rz\CkeditorBundle\Exception\ConfigManagerException;
+
+class CkeditorExtension extends \Twig_Extension
+{
+
+    protected $configManager;
+
+    public function __construct(ConfigManagerInterface $configManager)
+    {
+        $this->configManager = $configManager;
+    }
+
+    /**
+     *
+     * @access public
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            'rz_ckeditor_config' => new \Twig_Function_Method($this, 'getCKEditorConfig', array('is_safe' => array('html'))),
+        );
+    }
+
+    /**
+     *
+     * Truncates string if longer than needed and appends '...' to signify shorthand, otherwise returns original string.
+     *
+     * @access public
+     *
+     * @param $configIndex
+     * @param string $toolbarType
+     *
+     * @throws \Rz\Exception\ConfigManagerException
+     * @internal param $needle
+     * @internal param $haystack
+     *
+     * @return int
+     */
+    public function getCKEditorConfig($configIndex, $toolbarType = 'inline_editor')
+    {
+        $config = $this->configManager->getConfig($toolbarType);
+
+        if (!array_key_exists($configIndex, $config)) {
+            throw ConfigManagerException::configDoesNotExist($configIndex);
+        }
+
+        return $config[$configIndex];
+
+    }
+
+    /**
+     *
+     * @access public
+     * @return string
+     */
+    public function getName()
+    {
+        return 'rz_ckeditor';
+    }
+
+}
